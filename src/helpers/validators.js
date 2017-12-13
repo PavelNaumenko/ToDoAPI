79b5484c48f39efffbc83bcec4a74ef1083b6bc2
Delorian <axyebatop@gmail.com>
2017-12-14 00:13:18 +0200

    add new validators

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -11,6 +11,8 @@ const validateUser = (email, password) => {
   return true;
 };
 
+const isValidId = _id => /^[0-9a-fA-F]{24}$/.test(_id);
+
 const validateTask = (task) => {
   const { _id, title, completed = false } = task;
   if (!title) {
@@ -22,13 +24,50 @@ const validateTask = (task) => {
   if (typeof completed !== 'boolean') {
     throw new Error(`completed expected string but got ${typeof completed}`);
   }
-  if (_id && !/^[0-9a-fA-F]{24}$/.test(_id)) {
+  if (_id && !isValidId(_id)) {
     throw new Error('_id must be a string of 24 hex characters');
   }
   return true;
 };
 
+const validateTaskFilter = ({ userId, completed, created_at: createdAt, limit = 0, offset = 0 }) => {
+  const query = { filter: { userId } };
+  if (completed) {
+    if (completed !== 'true' && completed !== 'false') {
+      throw new Error(`completed expected boolean but got ${typeof completed}`);
+    }
+    query.filter.completed = completed === 'true';
+  }
+  query.sort = 1;
+  if (createdAt) {
+    const ca = parseInt(createdAt, 10);
+    if (Math.abs(ca) !== 1) {
+      throw new Error(`created_at expected 1 or -1 but got ${createdAt}`);
+    }
+    query.sort = ca > 0 ? 1 : -1;
+  }
+  const l = Number(limit);
+  if (Number.isNaN(l)) {
+    throw new Error(`limit expected number but got ${typeof limit}`);
+  }
+  if (l < 0) {
+    throw new Error('limit must be a great than 0');
+  }
+  query.limit = l;
+  const o = Number(offset);
+  if (Number.isNaN(o)) {
+    throw new Error(`limit expected number but got ${typeof offset}`);
+  }
+  if (o < 0) {
+    throw new Error('offset must be a great than 0');
+  }
+  query.offset = o;
+  return query;
+};
+
 module.exports = {
   validateUser,
   validateTask,
+  validateTaskFilter,
+  isValidId,
 };
